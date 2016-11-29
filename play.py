1bb71a6c24a3240dbc2078c39119a9dd982f4ad1
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-11-28 22:54:33 -0500

    Added custom lengths

--- play.py
+++ play.py
@@ -4,6 +4,7 @@ import pyaudio
 
 NUM_CHANNELS = 1
 RATE = 44100
+DEFAULT_LENGTH = 0.05
 
 def sine(frequency, length, rate):
 	length = int(length * rate)
@@ -22,10 +23,13 @@ def play_tone(stream, frequency=440, length=1, rate=RATE):
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32, channels=NUM_CHANNELS, rate=RATE, output=1)
 
-frequencies = [400,600,300,600,400,600,500,600,400,600,700,600]
+frequencies = [400,[600,0.1],300,[600,0.02],[400,0.02],[600,0.02],500,600,400,[600,0.08],[700,0.08],[600,0.08]]
 
 for frequency in frequencies:
-	play_tone(stream, frequency=frequency, length=0.05)
+	if isinstance(frequency, list): # Permit custom length
+		play_tone(stream, frequency=frequency[0], length=frequency[1])
+	else: # Use Default Length
+		play_tone(stream, frequency=frequency, length=DEFAULT_LENGTH)
 
 stream.close()
 p.terminate()
